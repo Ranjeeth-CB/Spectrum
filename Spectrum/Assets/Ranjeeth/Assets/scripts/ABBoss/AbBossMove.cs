f504b5f15f666b81567ec01aa32584f233f9481c
Ranjeeth <f20200316@pilani.bits-pilani.ac.in>
2022-03-23 23:33:51 +0530

    Game addded

---
+++ Spectrum/Assets/Ranjeeth/Assets/scripts/ABBoss/AbBossMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbBossMove : MonoBehaviour
+{
+    GameObject player;
+    public float speed = 1f;
+    Vector2 target;
+    public Transform targetPos1;
+    public Transform targetPos2;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player");
+
+        target = new Vector2(targetPos1.transform.position.x , targetPos1.transform.position.y);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y == targetPos2.position.y)
+        {
+            target = new Vector2(targetPos1.transform.position.x , targetPos1.transform.position.y);
+
+        }
+        else if(this.transform.position.y == targetPos1.position.y )
+        {
+            target = new Vector2(targetPos2.transform.position.x , targetPos2.transform.position.y);
+
+            
+
+        }
+        
+        transform.position = Vector2.MoveTowards(transform.position , target , speed);
+
+    
+        
+    }
+}
---
+++ Spectrum/Assets/Ranjeeth/Assets/scripts/ABBoss/End.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+    public GameObject Boss;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Boss.transform.position == target.position)
+        {
+            
+        }
+        
+    }
+}
